de23d121d11ecb7a07969ebcea5bb0f92f35f1a3
Amael Noel <noelama@gmail.com>
2016-03-08 11:32:40 -0400

    Use ExcelAsyncUtil from ExcelDna

--- AddinX.WPF.Contract/IWpfHelper.cs
+++ AddinX.WPF.Contract/IWpfHelper.cs
@@ -1,10 +1,9 @@
-﻿using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AddinX.Wpf.Contract
 {
     public interface IWpfHelper
     {
-        void Show(Window window, TaskCreationOptions taskOption = TaskCreationOptions.None);
+        void Show(Window window);
     }
 }
